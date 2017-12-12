102f0f1d1c5c0714547faf29ef316c4fcda8e069
Devin Nelson <dnelson2@lgsinnovations.com>
2017-12-12 11:00:43 -0500

    day ten part 2

--- 2017/DayTen/DayTen.java
+++ 2017/DayTen/DayTen.java
@@ -6,53 +6,89 @@ public class DayTen {
     private final static int LIST_SIZE = 256;
 
 	public static void main(String[] args) {
-        Integer[] lengths = getInput();
+        char[] input = getInput().toCharArray();
+        
+        List<Integer> lengthList = new ArrayList<>();
+        for (char c : input) {
+            lengthList.add((int) c);
+        }
+        lengthList.addAll(new ArrayList<Integer>(Arrays.asList(17, 31, 73, 47, 23)));
+        System.out.println("LENGTHS: ");
+        System.out.println(lengthList);
+        System.out.println("");
+        Integer[] lengths = lengthList.toArray(new Integer[0]);
+        
+
+
         Integer[] list = initList(LIST_SIZE);
         
         int index = 0;
         int skipSize = 0;
-        for (int length : lengths) {
-            System.out.println("Before: " + Arrays.asList(list));
-        
-        
-            Integer[] subList = null;
-            if ((index + length) > list.length) {
-                // Handle the more complicated wrapping case
-                // Yes, I'm sure I can combine these, but I'm feeling lazy. It'll probably bite me in part 2
-                int sizeToEnd = list.length - index;
-                int sizeFromBegin = length - sizeToEnd;
-                List<Integer> tmpList = new ArrayList<>();
-                tmpList.addAll(Arrays.asList(Arrays.copyOfRange(list, index, index + sizeToEnd)));
-                tmpList.addAll(Arrays.asList(Arrays.copyOfRange(list, 0, sizeFromBegin)));
-                subList = reverse(tmpList.toArray(new Integer[0]));
+        int round = 0;
+        while (round < 64) {
+            for (int length : lengths) {
+                if (skipSize == LIST_SIZE) {
+                    skipSize = 0;
+                }
+
+                Integer[] subList = null;
+                if ((index + length) > list.length) {
+                    // Handle the more complicated wrapping case
+                    // Yes, I'm sure I can combine these, but I'm feeling lazy. It'll probably bite me in part 2
+                    int sizeToEnd = list.length - index;
+                    int sizeFromBegin = length - sizeToEnd;
+                    List<Integer> tmpList = new ArrayList<>();
+                    tmpList.addAll(Arrays.asList(Arrays.copyOfRange(list, index, index + sizeToEnd)));
+                    tmpList.addAll(Arrays.asList(Arrays.copyOfRange(list, 0, sizeFromBegin)));
+                    subList = reverse(tmpList.toArray(new Integer[0]));
+                    
+                    for (int i = 0; i < sizeToEnd; i++) {
+                        list[index + i] = subList[i];
+                    }
+                    for (int i = 0; i < sizeFromBegin; i++) {
+                        list[0 + i] = subList[sizeToEnd + i];
+                    }
+                } else {
+                    // Handle the simple case where we don't wrap
+                    subList = Arrays.copyOfRange(list, index, index + length);
+                    subList = reverse(subList);
+                    for (int i = 0; i < subList.length; i++) {
+                        list[index + i] = subList[i];
+                    }
+                }
                 
-                for (int i = 0; i < sizeToEnd; i++) {
-                    list[index + i] = subList[i];
+                index += length + skipSize;
+                while (index > list.length) {
+                    index -= list.length;
                 }
-                for (int i = 0; i < sizeFromBegin; i++) {
-                    list[0 + i] = subList[sizeToEnd + i];
-                }
-            } else {
-                // Handle the simple case where we don't wrap
-                subList = Arrays.copyOfRange(list, index, index + length);
-                subList = reverse(subList);
-                for (int i = 0; i < subList.length; i++) {
-                    list[index + i] = subList[i];
-                }
+                skipSize++;
             }
-            
-            System.out.println("After: " + Arrays.asList(list));
-            System.out.println("");
-            
-            index += length + skipSize;
-            skipSize++;
-            if (index > list.length) {
-                index -= list.length;
-            }
+            round++;
         }
+        System.out.println("FINAL LIST: ");
+        System.out.println(Arrays.asList(list));
+        System.out.println("");
+    
+        // Create dense hash
+        List<Integer> denseHash = new ArrayList<>();
+        for (int block = 0; block < 16; block++) {
+            int num = list[16 * block];
+            for (int i = 1; i < 16; i++) {
+               num = num ^ list[i + (16 * block)]; 
+            }
+            denseHash.add(num);
+        }
+        System.out.println("DENSE HASH: " + denseHash);
+        System.out.println("");
+
+        // Create hex string
+        String finalHash = "";
+        for (int i : denseHash) {
+            finalHash = finalHash.concat(Integer.toHexString(i));
+        }
+        System.out.println("FINAL HASH: " + finalHash);
         
-        System.out.println("Answer: " + (list[0] * list[1]));
-	}
+    }
     
     private static Integer[] reverse(Integer[] subList) {
         Integer[] retVal = new Integer[subList.length];
@@ -71,18 +107,12 @@ public class DayTen {
         return list;
     }
 
-	private static Integer[] getInput() {
-	    try (BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"))) {
-            String line = bufferedReader.readLine();
-            int[] intArr = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
-            Integer[] input = new Integer[intArr.length];
-            for (int i = 0; i < intArr.length; i++) {
-                input[i] = Integer.valueOf(intArr[i]);
-            }
-            return input;
-		} catch (IOException e) {
-			System.out.println("uh oh");
-		}
-		return new Integer[0];
+    private static String getInput() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("input.txt"))) {
+            return bufferedReader.readLine();
+	} catch (IOException e) {
+	    System.out.println("uh oh");
 	}
+	return "";
+    }
 }
